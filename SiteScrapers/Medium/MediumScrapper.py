9249bf6f0598ae864a88c1061b1e0db88436591a
amiel <amielnoy@gmail.com>
2023-05-19 12:05:02 +0300

    finished medium method that print's article text and all it's images

--- SiteScrapers/Medium/MediumScrapper.py
+++ SiteScrapers/Medium/MediumScrapper.py
@@ -5,8 +5,9 @@ from bs4 import BeautifulSoup
 class MediumScrraper:
 
     @staticmethod
-    def getPostsFullText():
-        url = 'https://medium.com/@mythiliraju651/best-15-test-automation-trends-for-2023-75108209696f'
+    def getPostsFullTextAndMainImageUrl(url):
+
+        global size_string
         response = requests.get(url)
 
         # Parse the HTML content using BeautifulSoup
@@ -20,11 +21,35 @@ class MediumScrraper:
         for element in post_text_elements:
             post_text += element.text.strip() + " "
         print(post_text)
-        return post_text
+
+        #image_tags = soup.find_all("img",alt='')
+        image_tags = soup.find_all('img')
+        # Extract the URLs from the 'src' attribute of the image tags
+        image_urls_list = []
+        for tag in image_tags:
+            image_url = tag.get("src")
+            if image_url is None:
+                continue
+            else:
+               image_url_string=" \n "+str(image_url)+" \n "
+            #print(image_url_string)
+            if image_url_string is not None:
+                image_urls_list.append(image_url_string)
+
+        final_image_urls_list=[]
+        for url_string in image_urls_list:
+            if url_string != None:
+                final_image_urls_list.append(url_string)
+        #convert list to string and concatanate it to post text
+        article_and_images = 'article_text=\n' + post_text +" ".join(final_image_urls_list) + "\n"
+
+        print(article_and_images)
+        return article_and_images
 
     @staticmethod
     def getAllArticlesTitlesAndLinks():
         # URL of the MEDIUM website
+        global results_list
         url = "https://www.medium.com/"
 
         # Send a GET request to the URL
@@ -53,6 +78,7 @@ class MediumScrraper:
             print("An exception occurred:", error)  # An exception occurred:
 
         return results_list
+
     @staticmethod
     def scrape_image_urls():
         url = "https://www.medium.com/"
@@ -75,13 +101,15 @@ class MediumScrraper:
 
         return image_urls
 
+
 # URL of the Medium po
 
 
 if __name__ == "__main__":
-    # MediumScrraper.getPostsFullText()
-    #MediumScrraper.getAllArticlesTitlesAndLinks()
-    MediumScrraper.scrape_image_urls()
+    url = 'https://medium.com/@mythiliraju651/best-15-test-automation-trends-for-2023-75108209696f'
+    MediumScrraper.getPostsFullTextAndMainImageUrl(url)
+    # MediumScrraper.getAllArticlesTitlesAndLinks()
+    # MediumScrraper.scrape_image_urls()
     # article_url1='https://edition.cnn.com/2023/05/18/asia/g7-summit-japan-key-issues-analysis-intl-hnk/index.html'
     # MediumScrraper.scrape_article_text(article_url1)
     # article_url2 = "https://edition.cnn.com/2023/05/17/americas/harry-meghan-car-crash-intl/index.html"
