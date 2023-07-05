29d6a45237405628288ce8292224b0c6058a0f29
amiel <amielnoy@gmail.com>
2023-07-06 01:52:34 +0300

    remove scrraping calls in app

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import json
+from http.client import HTTPException
+
+from DbOperations.DbOperations import DbOperations
 from SiteScrapers import amazon
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request, redirect, url_for, render_template
 
 from SiteScrapers.IndeedScrraping.Indeed_positions_extraction import IndeedJobsExtraction
 from SiteScrapers.ebay_products_extraction import EbayProductsExtraction
@@ -7,58 +11,95 @@ from SiteScrapers.yelp_products_extraction import YelpRestaurantsExtraction
 
 app = Flask(__name__)
 
-
-# @pytest.fixture(scope="function")
-# def setup_browser_page(playwright: Playwright) -> Page:
-#     browser = playwright.chromium.launch(headless=False)
-#     context = browser.new_context()
-#     page = context.new_page()
-#     yield browser
-#     browser.close()
+is_logged_in = False
 
 
+@app.route('/get_user/?user_name=')
+def get_user():
+    db_ops = DbOperations()
+    db_ops.connect('UserMsgs.db')
+    query_result = db_ops.get_user()
+    return jsonify(query_result)
 
 
-@app.route('/amazon/searches')
-def get_amazon_searches():
-    return jsonify(amazon.get_amazon_search_phrases())
+# GET requests will be blocked
+@app.route('/register', methods=['POST'])
+def register_user():
+    request_data = request.get_json()
 
+    username = request_data['UserName']
+    password = request_data['Password']
 
-@app.route('/amazon/products')
-def get_amazon_products():
-    searches = amazon.get_amazon_search_phrases()
-    return jsonify(amazon.get_amazon_data(searches[0]))
+    db_ops = DbOperations()
+    db_ops.connect('UserMsgs.db')
+    # db_ops.insert_new_user(username, password)
+    db_ops.store_user_and_password_hash(username, password)
 
-
-# EBAY
-@app.route('/ebay/searches')
-def get_ebay_searches():
-    return jsonify(EbayProductsExtraction.get_ebay_search_phrases())
-
-
-# EBAY
-@app.route('/ebay/products')
-def get_ebay_products():
-    list_of_product_detail_items = EbayProductsExtraction.get_ebay_popular_product_details()
-    return jsonify(list_of_product_detail_items)
-
-
-# EBAY
-# EBAY
-@app.route('/yelp/Restaurants')
-def get_yelp_default_resurant_details():
-    restaurants_items_details_list = YelpRestaurantsExtraction.get_yelp_default_resurants_details()
-    return jsonify(restaurants_items_details_list)
-
-
-@app.route('/indeed/all_jobs')
-def get_indeed():
-    return IndeedJobsExtraction.get_jobs_playwright_on_chrome()
+    is_password_verrified = db_ops.verify_password(username, password)
+    if is_password_verrified:
+        print("Password=" + password + " VERIFIED!")
+    else:
+        print("Password=" + password + " NOT VVERIFIED!! ERROR!!")
+    return jsonify("Wrote to Users table: user_name=" + username + " Password=" + password)
 
 
 @app.errorhandler(404)
 def resource_not_found(e):
     return make_response(jsonify(error='Not found!'), 404)
+
+
+@app.errorhandler(401)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 401)
+
+
+@app.errorhandler(403)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 403)
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+
+# Login route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        username = request_data['UserName']
+        password = request_data['Password']
+        db_ops = DbOperations()
+        db_ops.connect('UserMsgs.db')
+
+        if db_ops.verify_password(username, password):
+            return redirect(url_for('home'))
+        else:
+            error = 'Invalid username or password.'
+            # return render_template('login.html', error=error)
+            return jsonify({'error': error}), 401
+    return render_template('login.html')
+
+
+# Home route (requires authentication)
+@app.route('/home')
+def home():
+    # Example: Retrieve user-specific data based on session
+    # user_id = session['user_id']
+    # Retrieve user data from the database based on user_id
+    # ...
+    return 'Welcome to the home page!'
 
 
 if __name__ == '__main__':
